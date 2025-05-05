8664ad1525bbd038418a5646b268be3e838fce80
Mihneo <jmgamer345@gmail.com>
2025-05-05 09:55:15 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using MockupApiTesting.ViewModel;
+
+namespace MockupApiTesting
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        private void SearchUserButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(UserIdEntry.Text))
+            {
+                DisplayAlert("Search Error", "Wrong ID, please input a number", "Sige Dol");
+            }
+            return;
+        }
+    }
+
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MockupApiTesting.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Users> users { get; set; }
+        HttpClient Client;
+        JsonSerializerOptions _serializerOptions;
+        string baseURl = "https://6807403ce81df7060eb95feb.mockapi.io";
+
+        public MainViewModel()
+        {
+            Client = new HttpClient();
+            users = new ObservableCollection<Users>();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+        }
+
+        public ICommand RetreiveAllUsers => new Command(async () =>
+        {
+            var url = $"{baseURl}/users";
+            var response = await Client.GetAsync(url);
+            var responseStream = await response.Content.ReadAsStreamAsync();
+            var data = await JsonSerializer.DeserializeAsync<List<Users>>(responseStream, _serializerOptions);
+
+            if (data != null)
+            {
+                users.Clear();
+                foreach (var user in data)
+                {
+                    users.Add(user);
+                }
+            }
+            // Logic to retrieve all users
+        });
+
+        public ICommand SearchUser => new Command(async (userId) =>
+        {
+            if (userId is not string userIdString || string.IsNullOrWhiteSpace(userIdString))
+            {
+                return;
+            }
+
+            var url = $"{baseURl}/users/{userId}";
+            var response = await Client.GetAsync(url);
+            var responseStream = await response.Content.ReadAsStreamAsync();
+            var searchedUser = await JsonSerializer.DeserializeAsync<Users>(responseStream, _serializerOptions);
+
+            if (searchedUser != null)
+            {
+                users.Clear();
+
+                users.Add(searchedUser);
+
+            }
+        });
+
+        public ICommand AddUser => new Command(async (name) =>
+        {
+            name = "asd";
+        });
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
